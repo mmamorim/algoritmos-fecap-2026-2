9bc106e74d7cf8c9097ca216d4ff5f1eda31121d
Marcelo <marc.m.amorim@gmail.com>
2026-09-04 22:33:36 -0300

    v1

---
+++ aula01/Program.cs
@@ -0,0 +1,11 @@
+﻿
+String nome;
+int idade;
+
+Console.WriteLine("Digite seu nome:");
+nome = Console.ReadLine();
+Console.WriteLine("Digite sua idade:");
+idade = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Oi "+nome+" tudo bem com vc?");
+Console.WriteLine("Vc tem "+idade+" anos.");
