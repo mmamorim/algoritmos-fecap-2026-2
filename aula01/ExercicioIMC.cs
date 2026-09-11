0493c078eea22cee024f096e314d2c2d8e675eda
Marcelo <marc.m.amorim@gmail.com>
2026-09-11 20:43:06 -0300

    v1

---
+++ aula01/ExercicioIMC.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Digite sua peso: ");
+double peso = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite sua altura: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double imc = peso / (altura*altura);
+
+Console.WriteLine("Seu IMC é "+imc);
