7dfcc1a17ce089735b43ee5bea5f3015938f43db
Marcelo <marc.m.amorim@gmail.com>
2026-09-11 22:02:29 -0300

    v1

---
+++ aula01/Exercicio03.cs
@@ -0,0 +1,12 @@
+﻿
+int numero, c, d, u;
+
+Console.WriteLine("Digite um numero com 3 digitos: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+c = numero / 100;
+u = numero % 10;
+d = (numero / 10) % 10;
+Console.WriteLine("Unidade: "+u);
+Console.WriteLine("Dezena: "+d);
+Console.WriteLine("Centena: "+c);
