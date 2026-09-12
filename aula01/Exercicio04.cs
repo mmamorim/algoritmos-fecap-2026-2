e9a0f4809137b1e4a6b7a0591ef2b0fd549061bb
Marcelo <marc.m.amorim@gmail.com>
2026-09-11 22:40:04 -0300

    v1

---
+++ aula01/Exercicio04.cs
@@ -0,0 +1,26 @@
+﻿
+int horaIni, horaFim, minIni, minFim;
+int qtdeMinInicio, qtdeMinFinal;
+
+Console.WriteLine("Digite a hora de início da reunião: ");
+horaIni = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite os minutos de início da reunião: ");
+minIni = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a hora de Término da reunião: ");
+horaFim = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite os minutos de Término da reunião: ");
+minFim = Convert.ToInt32(Console.ReadLine());
+
+qtdeMinInicio = horaIni*60 + minIni;
+qtdeMinFinal = horaFim*60 + minFim;
+//Console.WriteLine("qtdeMinInicio "+qtdeMinInicio);
+//Console.WriteLine("qtdeMinFinal "+qtdeMinFinal);
+int totalMin = qtdeMinFinal - qtdeMinInicio;
+int horas = totalMin / 60;
+int minutos = totalMin % 60;
+
+//Console.WriteLine("Tempo de duração "+horas+":"+minutos);
+Console.WriteLine($"Tempo de duração {horas}:{minutos}");
+
+
