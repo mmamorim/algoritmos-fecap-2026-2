5fba572c74baf6fc1246fa1b32e44894e58e9dbf
Marcelo <marc.m.amorim@gmail.com>
2026-09-11 21:22:06 -0300

    v1

---
+++ aula01/Exercicio01.cs
@@ -0,0 +1,11 @@
+﻿
+double ni, pi, provaFinal, media;
+
+Console.WriteLine("Digite sua nota NI:");
+ni = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite sua nota PI:");
+pi = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite sua nota Prova Final:");
+provaFinal = Convert.ToDouble(Console.ReadLine());
+media = ni*0.2 + pi*0.3 + provaFinal*0.5;
+Console.WriteLine("Sua média é "+media);
